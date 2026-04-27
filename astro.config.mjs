7acc6a5986537aa6a63be36ac64780398829f431
zakzakst <e1433104@gmail.com>
2026-04-27 17:51:50 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: 'https://zakzakst.github.io',
+  base: '/ai-practice',
   vite: {
     plugins: [tailwindcss()],
-    // NOTE: 下記エラー対応のため記載
-    // https://github.com/withastro/astro/issues/15857
     optimizeDeps: {
-      include: ["astro/toolbar"],
+      // NOTE: 下記エラー対応のため記載
+      // https://github.com/withastro/astro/issues/15857
       exclude: ["astro/runtime/client/dev-toolbar/entrypoint.js"],
     },
   },
