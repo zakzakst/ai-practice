dcd50fbe8267dae3b337afaa34c29fb44f1a78d5
zakzakst <e1433104@gmail.com>
2026-04-29 03:24:33 +0900

    fix

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  rules: {
+    "selector-class-pattern": null,
+    "keyframes-name-pattern": null,
+    "no-descending-specificity": null,
+  },
+};
